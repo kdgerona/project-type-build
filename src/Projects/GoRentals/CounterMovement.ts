5c38ea9f051520fd37eebb43b655c0a47dd8ed67
Samenodin Sarip <ssarip@dnamicro.com>
2021-12-09 07:00:05 +0800

    CorporateMovement, CounterMovement...

---
+++ src/Projects/GoRentals/CounterMovement.ts
@@ -0,0 +1,9 @@
+import { IEntitySchemaConfig } from '../../types';
+
+const  CounterMovement: IEntitySchemaConfig = {
+  entity: 'counter_movement',
+  schema_version: 'v4',
+  properties: []
+};
+
+export default  CounterMovement;
